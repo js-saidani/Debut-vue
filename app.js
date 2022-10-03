002f2d74e294cce69256bcd46b398658d1be751f
js-saidani <js.saidani@gmail.com>
2022-10-03 21:11:37 +0100

    first github

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const app = vue.createApp({
+  data() {
+    return {
+        icon: ' ',
+        autheur: 'sofiane',
+        age: 25,
+        bio:'un gars cool',
+    }
+  }
+})
+
+app.mount('#app'),
